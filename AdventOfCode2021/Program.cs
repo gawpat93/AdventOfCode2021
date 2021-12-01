a1d8a6d9706b58de8bb73652e5b2fba5ce42b5f3
Patryk Gawryszewski <patgaw09318@gmail.com>
2021-12-01 16:45:26 +0100

    refactoring

---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,7 @@
+﻿using AdventOfCode2021;
+
+var resultDay01_1 = Day01.CalculatePart1(@"Data\inputDay01.dat");
+Console.WriteLine($"Day01.1: {resultDay01_1}"); //1316
+
+var resultDay01_2 = Day01.CalculatePart2(@"Data\inputDay01.dat");
+Console.WriteLine($"Day01.2: {resultDay01_2}"); //1344
