4c29f77236c58161a85ab4be56572f9d2a68c1a9
Patryk Gawryszewski <patgaw09318@gmail.com>
2021-12-02 16:05:20 +0100

    Day02

---
+++ AdventOfCode2021/Day02.cs
@@ -0,0 +1,61 @@
+﻿namespace AdventOfCode2021
+{
+    public static class Day02
+    {
+        public static int CalculatePart1(string inputFileName)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+            var horizontal = 0;
+            var depth = 0;
+            foreach (var line in lines)
+            {
+                var elements = line.Split(' ');
+                var command = elements[0];
+                var value = int.Parse(elements[1]);
+                if (command == "forward")
+                {
+                    horizontal += value;
+                }
+                else if (command == "down")
+                {
+                    depth += value;
+                }
+                else if (command == "up")
+                {
+                    depth -= value;
+                }
+            }
+
+            return horizontal*depth;
+        }
+
+        public static int CalculatePart2(string inputFileName)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+            var horizontal = 0;
+            var depth = 0;
+            var aim = 0;
+            foreach (var line in lines)
+            {
+                var elements = line.Split(' ');
+                var command = elements[0];
+                var value = int.Parse(elements[1]);
+                if (command == "forward")
+                {
+                    horizontal += value;
+                    depth += value * aim;
+                }
+                else if (command == "down")
+                {
+                    aim += value;
+                }
+                else if (command == "up")
+                {
+                    aim -= value;
+                }
+            }
+
+            return horizontal * depth;
+        }
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -5,3 +5,9 @@ Console.WriteLine($"Day01.1: {resultDay01_1}"); //1316
 
 var resultDay01_2 = Day01.CalculatePart2(@"Data\inputDay01.dat");
 Console.WriteLine($"Day01.2: {resultDay01_2}"); //1344
+
+var resultDay02_1 = Day02.CalculatePart1(@"Data\inputDay02.dat");
+Console.WriteLine($"Day02.1: {resultDay02_1}"); //1815044
+
+var resultDay02_2 = Day02.CalculatePart2(@"Data\inputDay02.dat");
+Console.WriteLine($"Day02.2: {resultDay02_2}"); //1739283308
