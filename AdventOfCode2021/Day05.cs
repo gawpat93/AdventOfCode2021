17a197b050652c60f4e530b4a39e7853546a5ada
Patryk Gawryszewski <patgaw09318@gmail.com>
2021-12-05 14:18:21 +0100

    Day05 part1/2

---
+++ AdventOfCode2021/Day05.cs
@@ -0,0 +1,72 @@
+﻿namespace AdventOfCode2021
+{
+    public static class Day05
+    {
+        private class MarkedPoints
+        {
+            private readonly Dictionary<string, int> Points = new();
+
+            public int GetCriticalPointsNumber() => Points.Count(p => p.Value > 1);
+
+            public void MarkPoint(int x, int y)
+            {
+                var key = $"{x},{y}";
+                if (Points.ContainsKey(key))
+                {
+                    Points[key]++;
+                }
+                else
+                {
+                    Points.Add(key, 1);
+                }
+            }
+        }
+
+        private static int Solve(string inputFileName, bool withDiagonal = false)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+            var mps = new MarkedPoints();
+            foreach (var line in lines)
+            {
+                var points = line.Trim().Split("->");
+                var begin = points[0].Trim().Split(',').Select(int.Parse).ToArray();
+                var end = points[1].Trim().Split(',').Select(int.Parse).ToArray();
+                var x0 = begin[0];
+                var y0 = begin[1];
+                var x1 = end[0];
+                var y1 = end[1];
+                if (x0 == x1)
+                {
+                    for (var y = Math.Min(y0, y1); y <= Math.Max(y0, y1); y++)
+                    {
+                        mps.MarkPoint(x0, y);
+                    }
+                }
+                else if (y0 == y1)
+                {
+                    for (var x = Math.Min(x0, x1); x <= Math.Max(x0, x1); x++)
+                    {
+                        mps.MarkPoint(x, y0);
+                    }
+                }
+                else if (withDiagonal)
+                {
+                    var stepX = x0 < x1 ? 1 : -1;
+                    var stepY = y0 < y1 ? 1 : -1;
+                    for (var i = 0; i <= Math.Abs(x0 - x1); i++)
+                    {
+                        var x = x0 + i * stepX;
+                        var y = y0 + i * stepY;
+                        mps.MarkPoint(x, y);
+                    }
+                }
+            }
+
+            return mps.GetCriticalPointsNumber();
+        }
+
+        public static int CalculatePart1(string inputFileName) => Solve(inputFileName);
+
+        public static int CalculatePart2(string inputFileName) => Solve(inputFileName, true);
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,5 +1,5 @@
 ﻿using AdventOfCode2021;
-/*
+
 var resultDay01_1 = Day01.CalculatePart1(@"Data\inputDay01.dat");
 Console.WriteLine($"Day01.1: {resultDay01_1}"); //1316
 
@@ -17,9 +17,15 @@ Console.WriteLine($"Day03.1: {resultDay03_1}"); //845186
 
 var resultDay03_2 = Day03.CalculatePart2(@"Data\inputDay03.dat");
 Console.WriteLine($"Day03.2: {resultDay03_2}"); //4636702
-*/
+
 var resultDay04_1 = Day04.CalculatePart1(@"Data\inputDay04.dat");
 Console.WriteLine($"Day04.1: {resultDay04_1}"); //8442
 
 var resultDay04_2 = Day04.CalculatePart2(@"Data\inputDay04.dat");
 Console.WriteLine($"Day04.2: {resultDay04_2}"); //4590
+
+var resultDay05_1 = Day05.CalculatePart1(@"Data\inputDay05.dat");
+Console.WriteLine($"Day05.1: {resultDay05_1}"); //5835
+
+var resultDay05_2 = Day05.CalculatePart2(@"Data\inputDay05.dat");
+Console.WriteLine($"Day05.2: {resultDay05_2}"); //17013
